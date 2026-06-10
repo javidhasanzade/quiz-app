5f5f0b71ec71c70f2009d860d3a1985dc3c50bed
javidhasanzade <jh.grunger@gmail.com>
2026-06-10 16:10:56 +0200

    feat: implement dynamic question display and answer shuffling in QuestionsScreen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,21 +15,29 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'The questions will go here!',
-            style: TextStyle(fontSize: 24, color: Colors.white),
-          ),
-          const SizedBox(height: 30),
-          AnswerButton('Answer 1', () {}),
-          AnswerButton('Answer 2', () {}),
-          AnswerButton('Answer 3', () {}),
-          AnswerButton('Answer 4', () {}),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(height: 30),
+
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answer, () {});
+            }),
+          ],
+        ),
       ),
     );
   }
