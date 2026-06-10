67085bb4097d0377cfaeb06410563652aa3aea16
javidhasanzade <jh.grunger@gmail.com>
2026-06-10 15:43:34 +0200

    feat: add AnswerButton widget and update QuestionsScreen to use it

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -17,12 +18,15 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('The questions will go here!'),
+          const Text(
+            'The questions will go here!',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
           const SizedBox(height: 30),
-          ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
-          ElevatedButton(onPressed: () {}, child: const Text('Answer 2')),
-          ElevatedButton(onPressed: () {}, child: const Text('Answer 3')),
-          ElevatedButton(onPressed: () {}, child: const Text('Answer 4')),
+          AnswerButton('Answer 1', () {}),
+          AnswerButton('Answer 2', () {}),
+          AnswerButton('Answer 3', () {}),
+          AnswerButton('Answer 4', () {}),
         ],
       ),
     );
